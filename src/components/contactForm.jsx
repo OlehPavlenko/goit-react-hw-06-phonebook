150b96d3fb7995630030be69fbd5e5b5dc42cdcb
OlehPavlenko <pavlenko.zevs@gmail.com>
2022-09-25 18:12:42 +0300

    fix code

---
+++ src/components/contactForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { useDispatch } from 'react-redux';
+import { handleAddContact } from '../redux/store';
+
+const Form = () => {
+    const [contact, setContact] = useState({
+        id: nanoid(3),
+        name: '',
+        number: '',
+    });
+
+    const dispatch = useDispatch();
+
+    const onChange = e => {
+        const { name, value } = e.currentTarget;
+        setContact(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const clear = () => {
+        setContact({ id: nanoid(3), name: '', number: '' });
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        dispatch(handleAddContact(contact));
+        clear();
+    };
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>PhoneBook</h1>
+            <h3>Name</h3>
+            <input
+                type="text"
+                name="name"
+                value={contact.name}
+                // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                onChange={onChange}
+            />
+            <h3>Telephone number</h3>
+            <input
+                type="tel"
+                name="number"
+                value={contact.number}
+                // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                onChange={onChange}
+            />
+            <button type="submit">Add a contact</button>
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/components/markUp.jsx
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { handleRemoveContact } from '../redux/store';
+import React from 'react';
+
+const ContactsList = ({ filter }) => {
+    const contactsFromRedux = useSelector(state => state.contacts);
+    const dispatch = useDispatch();
+
+    return (
+        <ul className="contact_list">
+            {contactsFromRedux
+                .filter(({ name }) =>
+                    name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+                )
+                .map(({ id, name, number }) => {
+                    return (
+                        <li key={id} className="contact_item">
+                            {name} : {number}
+                            <button
+                                className="del-btn"
+                                onClick={() => {
+                                    dispatch(handleRemoveContact(id));
+                                }}
+                            >
+                                Delete
+                            </button>
+                        </li>
+                    );
+                })}
+        </ul>
+    );
+};
+
+export default ContactsList;
